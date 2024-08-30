5a642978f02e149489f2b44a23b1aea8fafb3ec9
huskydsb <huskydsb@outlook.com>
2024-08-31 06:40:28 +0800

    sc

--- Script/scamalytic.js
+++ Script/scamalytic.js
@@ -1,6 +1,5 @@
 console.log($environment.params);
-
-var ipUrl = "https://ipapi.co/json/";
+var ipUrl = "http://ip-api.com/json/";
 var scamUrl = "https://api11.scamalytics.com/shaoxinweixuer/?key=3d803bd1825826b88353d677e37d5f54ee5685e242347e88b8159c103bbc5ef1&ip=";
 
 var inputParams = $environment.params;
@@ -8,45 +7,30 @@ var nodeName = inputParams.node;
 
 var requestParams = {
     "url": ipUrl,
-    "node": nodeName, // 确保通过当前节点访问
-    "timeout": 60 // 增加超时时间至60秒
+    "node": nodeName
 };
 
-function requestWithRetry(params, callback, retries = 3) {
-    $httpClient.get(params, (error, response, data) => {
-        if (error && retries > 0) {
-            console.log(`请求失败，剩余重试次数: ${retries}, 错误信息: ` + JSON.stringify(error));
-            requestWithRetry(params, callback, retries - 1); // 重试
-        } else {
-            callback(error, response, data);
-        }
-    });
-}
-
-requestWithRetry(requestParams, (error, response, data) => {
+$httpClient.get(requestParams, (error, response, data) => {
     if (error) {
-        console.log("第一次请求失败：" + JSON.stringify(error)); // 输出第一次请求的错误信息
-        message = "<br><br>🔴 第一次请求超时";
+        var message = "<br><br>🔴 查询超时";
         message = `<p style="text-align: center; font-family: -apple-system; font-size: large; font-weight: bold;">${message}</p>`;
         $done({ "title": "IP洁净度检测", "htmlMessage": message });
     } else {
-        console.log("第一次请求成功：" + data); // 输出第一次请求的响应数据
+        console.log(data);
         var ipInfo = JSON.parse(data);
-        var ip = ipInfo.ip;
+        var ip = ipInfo.query; // 使用ip-api的查询结果
+
         var scamRequestParams = {
             "url": scamUrl + ip,
-            "node": nodeName, // 确保通过当前节点访问
-            "timeout": 60 // 增加超时时间至60秒
+            "node": nodeName
         };
 
-        requestWithRetry(scamRequestParams, (error, response, data) => {
+        $httpClient.get(scamRequestParams, (error, response, data) => {
             if (error) {
-                console.log("第二次请求失败：" + JSON.stringify(error)); // 输出第二次请求的错误信息
-                message = "<br><br>🔴 第二次请求超时";
+                var message = "<br><br>🔴 查询超时";
                 message = `<p style="text-align: center; font-family: -apple-system; font-size: large; font-weight: bold;">${message}</p>`;
                 $done({ "title": "IP洁净度检测", "htmlMessage": message });
             } else {
-                console.log("第二次请求成功：" + data); // 输出第二次请求的响应数据
                 var scamInfo = JSON.parse(data);
                 var scamDetails = `
                 <br><b>IP地址</b> : ${scamInfo.ip}
@@ -58,7 +42,7 @@ requestWithRetry(requestParams, (error, response, data) => {
                 <br><b>AS 编号</b> : ${scamInfo.as_number}
                 `;
 
-                message = `<p style="text-align: center; font-family: -apple-system; font-size: large; font-weight: thin">${scamDetails}</p>`;
+                var message = `<p style="text-align: center; font-family: -apple-system; font-size: large; font-weight: thin">${scamDetails}</p>`;
                 $done({ "title": "IP洁净度检测", "htmlMessage": message });
             }
         });
