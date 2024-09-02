e5e75732bc919865e2ef294199ef0e3250737574
huskydsb <huskydsb@outlook.com>
2024-09-03 00:52:22 +0800

    sc

--- Script/scamalytic.js
+++ Script/scamalytic.js
@@ -34,10 +34,30 @@ $httpClient.get(requestParams, (error, response, data) => {
                 var countryCode = scamInfo.ip_country_code;
                 var countryFlag = flags.get(countryCode) || '';
 
+                // 确定风险等级的 emoji 和描述
+                var riskemoji;
+                var riskDescription;
+                if (scamInfo.risk === 'very high') {
+                    riskemoji = '🔴'; // 代表非常高风险
+                    riskDescription = '非常高风险';
+                } else if (scamInfo.risk === 'high') {
+                    riskemoji = '🟠'; // 代表高风险
+                    riskDescription = '高风险';
+                } else if (scamInfo.risk === 'medium') {
+                    riskemoji = '🟡'; // 代表中等风险
+                    riskDescription = '中等风险';
+                } else if (scamInfo.risk === 'low') {
+                    riskemoji = '🟢'; // 代表低风险
+                    riskDescription = '低风险';
+                } else {
+                    riskemoji = '⚪'; // 未知风险
+                    riskDescription = '未知风险';
+                }
+
                 var scamDetails = `
                     <br><b>IP地址：</b>${scamInfo.ip}
                     <br><b>IP欺诈分数：</b>${scamInfo.score}
-                    <br><b>IP风险等级：</b>${scamInfo.risk === 'low' ? '低风险' : '高风险'}
+                    <br><b>IP风险等级：</b>${riskemoji} ${riskDescription}
                     <br><b>IP城市：</b>${scamInfo.ip_city}
                     <br><b>IP国家：</b>${countryFlag} ${countryCode}
                     <br><b>ISP名称：</b>${scamInfo['ISP Name']}
@@ -47,25 +67,25 @@ $httpClient.get(requestParams, (error, response, data) => {
                 `;
 
                 var resultHtml = `
-                    -------------------------------
-
-                    <br><b>IP地址：</b><span style="color: red;">${scamInfo.ip}</span>
-
-                    <br><b>IP欺诈分数：</b>${scamInfo.score}
-                    <br><b>IP风险等级：</b>${scamInfo.risk === 'low' ? '低风险' : '高风险'}
-
-                    <br><b>IP城市：</b>${scamInfo.ip_city}
-                    <br><b>IP国家：</b>${countryFlag} ${countryCode}
-
-                    <br><b>ISP名称：</b>${scamInfo['ISP Name']}
-                    <br><b>ISP欺诈分数：</b>${scamInfo['ISP Fraud Score']}
-
-                    <br><b>ASN编号：</b>${scamInfo.as_number}
-                    <br><b>ASN机构：</b>${scamInfo['Organization Name']}
-
-                    <br>-------------------------------
-                    <br><font color="red"><b>节点：</b> ➟ ${nodeName}</font>
-                `;
+                -------------------------------
+                <br><br> <!-- 空行 -->
+                <span style="color: red;"><b>IP地址：</b></span><span style="color: red;">${scamInfo.ip}</span>
+                <br><br> <!-- 空行 -->
+                <br><b>IP欺诈分数：</b>${scamInfo.score}
+                <br><b>IP风险等级：</b>${riskemoji} ${riskDescription}
+                <br><br> <!-- 空行 -->
+                <br><b>IP城市：</b>${scamInfo.ip_city}
+                <br><b>IP国家：</b>${countryFlag} ${countryCode}
+                <br><br> <!-- 空行 -->
+                <br><b>ISP名称：</b>${scamInfo['ISP Name']}
+                <br><b>ISP欺诈分数：</b>${scamInfo['ISP Fraud Score']}
+                <br><br> <!-- 空行 -->
+                <br><b>ASN编号：</b>${scamInfo.as_number}
+                <br><b>ASN机构：</b>${scamInfo['Organization Name']}
+                <br><br> <!-- 空行 -->
+                <br>-------------------------------
+                <br><font color="red"><b>节点：</b> ➟ ${nodeName}</font>
+            `;
 
                 var message = `<p style="text-align: center; font-family: -apple-system; font-size: large; font-weight: thin">${resultHtml}</p>`;
                 $done({ "title": "IP纯净度检测", "htmlMessage": message });
@@ -73,6 +93,7 @@ $httpClient.get(requestParams, (error, response, data) => {
         });
     }
 });
+
 
 var flags = new Map([
     ["AC", "🇦🇨"], ["AE", "🇦🇪"], ["AF", "🇦🇫"], ["AI", "🇦🇮"], ["AL", "🇦🇱"], ["AM", "🇦🇲"], ["AQ", "🇦🇶"], ["AR", "🇦🇷"], ["AS", "🇦🇸"], ["AT", "🇦🇹"], ["AU", "🇦🇺"], ["AW", "🇦🇼"], ["AX", "🇦🇽"], ["AZ", "🇦🇿"],
