5d9d210eaf3eef1e223a752ffd6b7ff18632ce41
huskydsb <huskydsb@outlook.com>
2024-09-13 12:36:35 +0800

    NG

--- Script/Nicegram.js
+++ Script/Nicegram.js
@@ -1,3 +1,9 @@
+[rewrite_local]
+^https?:\/\/nicegram\.cloud\/api\/v\d\/(ai-assistant\/purchase-list|user\/info|telegram\/auth) url script-response-body https://raw.githubusercontent.com/huskydsb/Loon/main/Script/Nicegram.js
+
+[mitm]
+hostname = nicegram.cloud
+*/
 var body = $response.body;
 
 body = body.replace(/"store_subscription":false/g, '"store_subscription":true');
