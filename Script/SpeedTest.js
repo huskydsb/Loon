5fcf69acd6c942d4c247011a79b24425d7da7b74
huskydsb <huskydsb@outlook.com>
2024-09-14 07:57:37 +0800

    st

--- Script/SpeedTest.js
+++ Script/SpeedTest.js
@@ -66,17 +66,17 @@ $httpClient.get({
                 $done({
                     title: "网络速度测试结果",
                     htmlMessage: `
-                        <p style="text-align: center; font-family: -apple-system; font-size: large; font-weight: thin;">
-                            <hr style="border: 1px solid #888;" />
-                            <br>-------------------------------------------------------
-                            <div style="display: flex; justify-content: center; align-items: center;">
-                                <div style="margin-right: 20px;"><b>网络延迟：</b>${pingDuration} ms</div>
-                                <div><b>下载速度：</b>${downloadSpeed} MB/s</div>
-                            </div>
-                            <br>-------------------------------------------------------
-                            <br><br>
-                            <span style="color: red; text-align: center; display: block;"><b>当前节点：</b>${nodeName}</span>
-                        </p>`,
+                    <p style="text-align: center; font-family: -apple-system; font-size: large; font-weight: thin;">
+                    <br>-------------------------------------------------------
+                    <br>
+                    <div style="display: flex; justify-content: center; align-items: center; margin: 0 auto;">
+                    <div style="margin-right: 20px;"><b>网络延迟：</b>${pingDuration} ms</div>
+                    <div><b>下载速度：</b>${downloadSpeed} MB/s</div>
+                    </div>
+                    <br>-------------------------------------------------------
+                    <br><br>
+                    <span style="color: red; text-align: center; display: block;"><b>当前节点：</b>${nodeName}</span>
+                    </p>`,
                     icon: "network",
                     "icon-color": "#5AC8FA"
                 });
