c7ee072c1b0e44132c0f27951672b6f4904cc6ab
huskydsb <huskydsb@outlook.com>
2024-09-13 12:17:12 +0800

    NG

--- Script/Nicegram.js
+++ Script/Nicegram.js
@@ -1,10 +1,10 @@
-// Loon 脚本
+
 let body = $response.body;
 
-// 替换订阅状态
+
 body = body.replace(/"store_subscription":false/g, '"store_subscription":true');
 body = body.replace(/"lifetime_subscription":false/g, '"lifetime_subscription":true');
 body = body.replace(/"subscription":false/g, '"subscription":true');
 
-// 完成并返回修改后的 body
+
 $done({ body });
