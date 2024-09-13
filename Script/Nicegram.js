0a5337beda5b4962359cebe88b49cffd117ba733
huskydsb <huskydsb@outlook.com>
2024-09-13 12:21:51 +0800

    ng

--- Script/Nicegram.js
+++ Script/Nicegram.js
@@ -1,12 +1,11 @@
 [mitm]
 hostname = nicegram.cloud
 
-let body = $response.body;
-
+var body = $response.body;
 
 body = body.replace(/"store_subscription":false/g, '"store_subscription":true');
-body = body.replace(/"lifetime_subscription":false/g, '"lifetime_subscription":true');
-body = body.replace(/"subscription":false/g, '"subscription":true');
+body = body.replace(/"lifetime_subscription":false/g,'"lifetime_subscription":true');
+body = body.replace(/"subscription":false/g,'"subscription":true');
 
 
 $done({ body });
